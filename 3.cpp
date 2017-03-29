cff4d599fe30ba362216d1293751a3330e35f9af
Bartek <bartek9393@poczta.onet.pl>
2017-03-29 15:36:05 +0100

    Dodano zadanie nr 3

---
+++ 3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "math.h"
+
+using namespace std;
+
+int main() {
+    unsigned long long liczba;
+    cin >> liczba;
+
+    if (liczba < 0 || liczba > pow(10, 18)) {
+        cout << "Liczba ze zlego zakresu";
+        return 0;
+    }
+
+    while (liczba) {
+        if (liczba % 10 != 4 && liczba % 10 != 7) {
+            cout << "NIE";
+            return 0;
+        }
+        liczba /= 10;
+    }
+    cout << "TAK";
+
+    return 0;
+}
