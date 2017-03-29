7d9c688a364e33addefe137ddaa91122ab28bc0b
Bartek <bartek9393@poczta.onet.pl>
2017-03-29 21:31:13 +0100

    Przed zmianą

--- 1.cpp
+++ 1.cpp
@@ -3,27 +3,37 @@
 using namespace std;
 
 int main() {
-    int n = 8, pomocnicza, wierszOdKonca, iloscZnakowX;
-    char tablica[n][n];
+    int n;
+    cout << "Podaj liczbe z przedzialu <3; 80>" << endl;
+    cin >> n;
+
+    if (n < 3 || n > 80) {
+        cout << "Nie podales liczby z przedzialu <3; 80>";
+        return 1;
+    }
+
+    int pomocnicza, wierszOdKonca, iloscZnakowX;
+
+    char amulet[n][n];
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            tablica[i][j] = '.';
+            amulet[i][j] = '.';
         }
     }
 
-    if (n % 2 != 0) {
+//    if (n % 2 != 0) {
         iloscZnakowX = 1;
         pomocnicza = n / 2;
         for (int k = 0; k < n / 2; ++k) {
             for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
-                tablica[k][i] = 'X';
+                amulet[k][i] = 'X';
             }
             iloscZnakowX += 2;
             pomocnicza--;
         }
 
         for (int l = 0; l < n; ++l) {
-            tablica[n / 2][l] = 'X';
+            amulet[n / 2][l] = 'X';
         }
         pomocnicza = n / 2;
         iloscZnakowX = 1;
@@ -31,18 +41,18 @@ int main() {
 
         for (int k = n / 2; k > 0; --k) {
             for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
-                tablica[wierszOdKonca][i] = 'X';
+                amulet[wierszOdKonca][i] = 'X';
             }
             iloscZnakowX += 2;
             pomocnicza--;
             wierszOdKonca--;
         }
-    } else {
+//    } else {
         iloscZnakowX = 2;
         pomocnicza = n / 2 - 1;
         for (int k = 0; k < n / 2; ++k) {
             for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
-                tablica[k][i] = 'X';
+                amulet[k][i] = 'X';
             }
             iloscZnakowX += 2;
             pomocnicza--;
@@ -50,10 +60,11 @@ int main() {
 
         for (int l = 0; l < n; ++l) {
             for (int i = 0; i < 2; ++i) {
-                tablica[(n / 2) - i][l] = 'X';
+                amulet[(n / 2) - i][l] = 'X';
 
             }
         }
+        iloscZnakowX = n % 2 == 0 ? 1 : 2;
 
         iloscZnakowX = 2;
         pomocnicza = n / 2 - 1;
@@ -61,17 +72,17 @@ int main() {
 
         for (int k = n / 2; k > 0; --k) {
             for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
-                tablica[wierszOdKonca][i] = 'X';
+                amulet[wierszOdKonca][i] = 'X';
             }
             iloscZnakowX += 2;
             pomocnicza--;
             wierszOdKonca--;
         }
-    }
+//    }
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cout << tablica[i][j];
+            cout << amulet[i][j];
         }
         cout << endl;
     }
