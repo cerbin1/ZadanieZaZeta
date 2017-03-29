d9860b3dab81fe990546bad4fecaa2a58fb8ef68
Bartek <bartek9393@poczta.onet.pl>
2017-03-29 19:07:14 +0100

    Oczyszczono kod

--- 1.cpp
+++ 1.cpp
@@ -3,75 +3,75 @@
 using namespace std;
 
 int main() {
-    int n = 8;
-    char array[n][n];
+    int n = 8, pomocnicza, wierszOdKonca, iloscZnakowX;
+    char tablica[n][n];
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            array[i][j] = '.';
+            tablica[i][j] = '.';
         }
     }
 
     if (n % 2 != 0) {
-        int dupa = 1;
-        int foo = n / 2;
+        iloscZnakowX = 1;
+        pomocnicza = n / 2;
         for (int k = 0; k < n / 2; ++k) {
-            for (int i = foo; i < dupa + foo; ++i) {
-                array[k][i] = 'X';
+            for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
+                tablica[k][i] = 'X';
             }
-            dupa += 2;
-            foo--;
+            iloscZnakowX += 2;
+            pomocnicza--;
         }
 
         for (int l = 0; l < n; ++l) {
-            array[n / 2][l] = 'X';
+            tablica[n / 2][l] = 'X';
         }
-        foo = n / 2;
-        dupa = 1;
-        int dd = n - 1;
+        pomocnicza = n / 2;
+        iloscZnakowX = 1;
+        wierszOdKonca = n - 1;
 
         for (int k = n / 2; k > 0; --k) {
-            for (int i = foo; i < dupa + foo; ++i) {
-                array[dd][i] = 'X';
+            for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
+                tablica[wierszOdKonca][i] = 'X';
             }
-            dupa += 2;
-            foo--;
-            dd--;
+            iloscZnakowX += 2;
+            pomocnicza--;
+            wierszOdKonca--;
         }
     } else {
-        int dupa = 2;
-        int foo = n / 2 - 1;
+        iloscZnakowX = 2;
+        pomocnicza = n / 2 - 1;
         for (int k = 0; k < n / 2; ++k) {
-            for (int i = foo; i < dupa + foo; ++i) {
-                array[k][i] = 'X';
+            for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
+                tablica[k][i] = 'X';
             }
-            dupa += 2;
-            foo--;
+            iloscZnakowX += 2;
+            pomocnicza--;
         }
 
         for (int l = 0; l < n; ++l) {
             for (int i = 0; i < 2; ++i) {
-                array[(n / 2) - i][l] = 'X';
+                tablica[(n / 2) - i][l] = 'X';
 
             }
         }
 
-        dupa = 2;
-        foo = n / 2 - 1;
-        int dd = n - 1;
+        iloscZnakowX = 2;
+        pomocnicza = n / 2 - 1;
+        wierszOdKonca = n - 1;
 
         for (int k = n / 2; k > 0; --k) {
-            for (int i = foo; i < dupa + foo; ++i) {
-                array[dd][i] = 'X';
+            for (int i = pomocnicza; i < iloscZnakowX + pomocnicza; ++i) {
+                tablica[wierszOdKonca][i] = 'X';
             }
-            dupa += 2;
-            foo--;
-            dd--;
+            iloscZnakowX += 2;
+            pomocnicza--;
+            wierszOdKonca--;
         }
     }
 
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
-            cout << array[i][j];
+            cout << tablica[i][j];
         }
         cout << endl;
     }
@@ -129,8 +129,6 @@ int main() {
     }*//*
 
 }*/
-
-#include<iostream>
 
 /*
 using namespace std;
