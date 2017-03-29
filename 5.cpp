55ce8f38e4569f365cefa179354b31923a879ef0
Bartek <bartek9393@poczta.onet.pl>
2017-03-29 16:18:28 +0100

    Dodano 5 zadanie

---
+++ 5.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int liczba;
+    cout << "Podaj liczbe z przedzialu <0; 1000>" << endl;
+    cin >> liczba;
+
+    if (liczba < 1 || liczba > 1000) {
+        cout << "Niepoprawna liczba. Przedzial <0; 1000>";
+        return 0;
+    }
+
+    bool czyJestPierwsza;
+    int iloscLiczbPierwszych = 0;
+
+    for (int i = 2; i < liczba; ++i) {
+        czyJestPierwsza = true;
+        for (int j = 2; j < i; ++j) {
+            if (i % j == 0) {
+                czyJestPierwsza = false;
+                break;
+            }
+        }
+        if (czyJestPierwsza) {
+            iloscLiczbPierwszych++;
+        }
+    }
+
+    cout << iloscLiczbPierwszych;
+
+    return 0;
+}
